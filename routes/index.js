160e1b35b2f4548293f857fba8325e3937eba293
Tolleiv Nietsch <info@tolleiv.de>
2015-07-15 23:48:31 +0200

    Add few statistics to the index page

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
+var statsGraph = require('../lib/stats');
 
-/* GET home page. */
 router.get('/', function (req, res, next) {
-    res.render('index', {title: 'Express'});
+    statsGraph.nodeCounts(function(err, results) {
+        res.render('index', {title: 'Mnfst', stats: results});
+    });
 });
 
 module.exports = router;
