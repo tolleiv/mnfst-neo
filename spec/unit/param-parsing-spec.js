2b3ea012513ee16dc55b094ade83469f63615ab5
Tolleiv Nietsch <tolleiv.nietsch@aoe.com>
2015-07-17 12:11:10 +0200

    Transform tests slightly to move the boilerplate stuff into a function

--- spec/unit/param-parsing-spec.js
+++ spec/unit/param-parsing-spec.js
@@ -3,51 +3,57 @@ var httpMocks = require('node-mocks-http');
 
 describe('the parameter parsing', function () {
 
-    it('transforms a simple file list to an array', function (done) {
+    function assertParserResult() {
+        var type = arguments.length == 2 ? '*' : arguments[0];
+        var input = arguments.length == 2 ? arguments[0] : arguments[1];
+        var expected = arguments.length == 2 ? arguments[1] : arguments[2];
+
         var request = httpMocks.createRequest({
-            body: "file1\nfile2\nfile3"
+            headers: {'Content-Type': type},
+            body: input
         });
         var response = httpMocks.createResponse();
-        expect(parser(request, response).value()).toEqual([{file: 'file1'}, {file: 'file2'}, {file: 'file3'}]);
+        expect(parser(request, response).value()).toEqual(expected);
+    }
+
+    it('transforms a simple file list to an array', function (done) {
+        assertParserResult(
+            "file1\nfile2\nfile3",
+            [{file: 'file1'}, {file: 'file2'}, {file: 'file3'}]
+        );
         done();
     });
 
     it('does not transform passed in arrays', function (done) {
-        var request = httpMocks.createRequest({
-            body: ['file4', 'file5']
-        });
-        var response = httpMocks.createResponse();
-        expect(parser(request, response).value()).toEqual([{file: 'file4'}, {file: 'file5'}]);
+        assertParserResult(
+            ['file4', 'file5'],
+            [{file: 'file4'}, {file: 'file5'}]
+        );
         done();
     });
 
     it('filters out empty values in text arguments', function (done) {
-        var request = httpMocks.createRequest({
-            body: "file7\n\nfile8\n"
-        });
-        var response = httpMocks.createResponse();
-        expect(parser(request, response).value()).toEqual([{file: 'file7'}, {file: 'file8'}]);
+        assertParserResult(
+            "file7\n\nfile8\n",
+            [{file: 'file7'}, {file: 'file8'}]
+        );
         done();
     });
 
     it('filters out empty values in array arguments', function (done) {
-        var request = httpMocks.createRequest({
-            body: ['file9', '', 'file10', '']
-        });
-        var response = httpMocks.createResponse();
-        expect(parser(request, response).value()).toEqual([{file: 'file9'}, {file: 'file10'}]);
+        assertParserResult(
+            ['file9', '', 'file10', ''],
+            [{file: 'file9'}, {file: 'file10'}]
+        );
         done();
     });
 
     it('transforms the extended file list to an array', function (done) {
-        var request = httpMocks.createRequest({
-            headers: {'Content-Type': 'text/csv'},
-            body: "file1\tService[apache2]\nfile2\tExec[run]"
-        });
-        var response = httpMocks.createResponse();
-
-        var assumption = [{file: 'file1', resource: 'Service[apache2]'}, {file: 'file2', resource: 'Exec[run]'}];
-        expect(parser(request, response).value()).toEqual(assumption);
+        assertParserResult(
+            'text/csv',
+            "file1\tService[apache2]\nfile2\tExec[run]",
+            [{file: 'file1', resource: 'Service[apache2]'}, {file: 'file2', resource: 'Exec[run]'}]
+        );
         done();
     });
 });
